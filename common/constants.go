96df8cd6c581ceea19471e7a04438bf5f2986dea
nidhey27 <nidhey.indurkar@infracloud.io>
2023-12-22 17:55:08 +0530

    update: remove magic values, create constants
    Signed-off-by: nidhey27 <nidhey.indurkar@infracloud.io>

---
+++ common/constants.go
@@ -0,0 +1,12 @@
+package common
+
+const (
+	IS          = " is "
+	SPACE       = " "
+	WRONG       = "I have no idea what you are talking about"
+	CREDITS     = "Credits"
+	CREDITS_THAN = " has more Credits than "
+	CREDITS_LESS = " has less Credits than "
+	LARGER_THAN  = " larger than "
+	SMALLER_THAN = " smaller than "
+)
--- helpers/calculate.go
+++ helpers/calculate.go
@@ -5,28 +5,18 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/nidhey27/coffee-assignment/common"
 	"github.com/nidhey27/coffee-assignment/utils"
-)
-
-const (
-	is          = " is "
-	space       = " "
-	wrong       = "I have no idea what you are talking about"
-	credits     = "Credits"
-	creditsThan = " has more Credits than "
-	creditsLess = " has less Credits than "
-	largerThan  = " larger than "
-	smallerThan = " smaller than "
 )
 
 func CalculateHowMuch(input string, romanMap map[string]string, valueMap map[string]float64) string {
 	result := ""
 	tempRoman := ""
 	tempQuestion := ""
-	if strings.Contains(input, is) {
-		splitted := strings.Split(input, is)
+	if strings.Contains(input, common.IS) {
+		splitted := strings.Split(input, common.IS)
 
-		splittedValue := strings.Split(splitted[1], space)
+		splittedValue := strings.Split(splitted[1], common.SPACE)
 
 		for index, value := range splittedValue {
 			_, ok := romanMap[value]
@@ -47,7 +37,7 @@ func CalculateHowMuch(input string, romanMap map[string]string, valueMap map[str
 		}
 		result = tempQuestion
 	} else {
-		result = wrong
+		result = common.WRONG
 	}
 	return result
 }
@@ -57,10 +47,10 @@ func CalculateHowMany(input string, romanMap map[string]string, valueMap map[str
 	tempRoman := ""
 	tempQuestion := ""
 
-	if strings.Contains(input, is) {
-		splitted := strings.Split(input, is)
+	if strings.Contains(input, common.IS) {
+		splitted := strings.Split(input, common.IS)
 
-		splittedValue := strings.Split(splitted[1], space)
+		splittedValue := strings.Split(splitted[1], common.SPACE)
 		for index, value := range splittedValue {
 			_, ok := romanMap[value]
 			if ok {
@@ -78,7 +68,7 @@ func CalculateHowMany(input string, romanMap map[string]string, valueMap map[str
 						finalNumeral := float64(numeral) * valueMap[value]
 
 						tempQuestion += value
-						tempQuestion += fmt.Sprintf(" is %f %s", finalNumeral, credits)
+						tempQuestion += fmt.Sprintf(" is %f %s", finalNumeral, common.CREDITS)
 					} else {
 						tempQuestion += " "
 					}
@@ -87,25 +77,25 @@ func CalculateHowMany(input string, romanMap map[string]string, valueMap map[str
 		}
 		result = tempQuestion
 	} else {
-		result = wrong
+		result = common.WRONG
 	}
 	return result
 }
 
 func ReturnWrong() string {
-	result := wrong
+	result := common.WRONG
 	return result
 }
 
 func StoreValueMap(input string, romanMap map[string]string) (string, float64) {
-	splitted := strings.Split(input, is)
-	splittedRoman := strings.Split(splitted[0], space)
+	splitted := strings.Split(input, common.IS)
+	splittedRoman := strings.Split(splitted[0], common.SPACE)
 
 	tempRoman := ""
 	key := ""
 	value := 0.00
 
-	credits := strings.Split(splitted[1], space)
+	credits := strings.Split(splitted[1], common.SPACE)
 	creditValue, err := strconv.Atoi(credits[0])
 	if err != nil {
 		return err.Error(), 0
@@ -131,7 +121,7 @@ func StoreValueMap(input string, romanMap map[string]string) (string, float64) {
 func StoreRomanMap(input string) (string, string) {
 	var key string = ""
 	var value string = ""
-	splitted := strings.Split(input, is)
+	splitted := strings.Split(input, common.IS)
 	if len(splitted) > 1 {
 		roman := strings.TrimSpace(splitted[1])
 		key = splitted[0]
@@ -169,8 +159,8 @@ func HasMore(input string, romanMap map[string]string, valueMap map[string]float
 	metal1 := ""
 	metal2 := ""
 
-	if strings.Contains(input, creditsThan) {
-		metal1, metal2, _ = extractMetalNames(creditsThan, input)
+	if strings.Contains(input, common.CREDITS_THAN) {
+		metal1, metal2, _ = extractMetalNames(common.CREDITS_THAN, input)
 
 		credits1 := valueMap[metal1]
 		credits2 := valueMap[metal2]
@@ -192,8 +182,8 @@ func HasLess(input string, romanMap map[string]string, valueMap map[string]float
 	metal1 := ""
 	metal2 := ""
 
-	if strings.Contains(input, creditsLess) {
-		metal1, metal2, _ = extractMetalNames(creditsLess, input)
+	if strings.Contains(input, common.CREDITS_LESS) {
+		metal1, metal2, _ = extractMetalNames(common.CREDITS_LESS, input)
 
 		credits1 := valueMap[metal1]
 		credits2 := valueMap[metal2]
@@ -214,9 +204,9 @@ func LargerThan(input string, romanMap map[string]string) string {
 	var metal1 []string
 	var metal2 []string
 
-	if strings.Contains(input, largerThan) {
+	if strings.Contains(input, common.LARGER_THAN) {
 		// Extract metal names
-		metal1, metal2, _ = extractRomanNames(largerThan, input, romanMap)
+		metal1, metal2, _ = extractRomanNames(common.LARGER_THAN, input, romanMap)
 
 		credits1 := 0
 		credits2 := 0
@@ -246,9 +236,9 @@ func SmallerThan(input string, romanMap map[string]string) string {
 	var metal1 []string
 	var metal2 []string
 
-	if strings.Contains(input, smallerThan) {
+	if strings.Contains(input, common.SMALLER_THAN) {
 		// Extract metal names
-		metal1, metal2, _ = extractRomanNames(smallerThan, input, romanMap)
+		metal1, metal2, _ = extractRomanNames(common.SMALLER_THAN, input, romanMap)
 
 		credits1 := 0
 		credits2 := 0
